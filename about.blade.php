760557b74ab6700b865b76430c233e82e75651c6
hzmij <82014512+hzmij@users.noreply.github.com>
2021-04-20 22:55:02 +0800

    Add files via upload

---
+++ about.blade.php
@@ -0,0 +1,6 @@
+@extends('app')
+@section('title', 'About Us')
+@section('content')
+ <h1> E-book Sharing Application </h1>
+ <p> E-book Sharing Application was established in 2020.</p>
+@endsection 
---
+++ catController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class catController extends Controller
+{
+    class catController extends Controller
+    {
+     public function about () {
+     return view('about');
+     }
+     public function team () {
+     return view('team');
+     }
+     public function services () {
+     $services = array(
+     'Service 1',
+     'Service 2'
+     );
+     return view('services', compact('services'));
+     }
+    } 
+}
